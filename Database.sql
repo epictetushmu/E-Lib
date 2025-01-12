05241311b587c3b0bf0bf57f229a47a03b29668d
Nikitas Menounos <menounosnikitas@gmail.com>
2025-01-12 14:38:40 +0200

    update db

--- Database.sql
+++ Database.sql
@@ -1,22 +1,32 @@
+-- Create the database with a name
+CREATE DATABASE LibraryDB 
+    DEFAULT CHARACTER SET = 'utf8mb4';
+
+-- Select the database
+USE LibraryDB;
+
+-- Create the Category table
 CREATE TABLE Category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
+-- Create the Book table
 CREATE TABLE Book (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     publication_year INT,
-    condition VARCHAR(10),
+    `condition` VARCHAR(10),
     number_of_copies INT,
-    description VARCHAR(255)
+    description VARCHAR(255),
     category_id INT,
     FOREIGN KEY (category_id) REFERENCES Category(id)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
+-- Create the Borrow table
 CREATE TABLE Borrow (
     id INT AUTO_INCREMENT PRIMARY KEY,
     book_id INT NOT NULL,
