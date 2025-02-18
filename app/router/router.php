5da2fd59a0ef7a18e0a466022cd73abbdd8c558b
nikmen <menounosnikitas@gmail.com>
2025-02-18 19:45:06 +0200

    feat: add .htaccess for URL rewriting and update Router to handle dynamic base URL

--- app/router/router.php
+++ app/router/router.php
@@ -5,10 +5,12 @@ require_once(__DIR__ . '/PageRouter.php');
 class Router {
     private $apiRouter;
     private $pageRouter;
+    private $baseUrl;
 
-    public function __construct() {
+    public function __construct($baseUrl = '/E-Lib') {
         $this->apiRouter = new ApiRouter();
         $this->pageRouter = new PageRouter();
+        $this->baseUrl = $baseUrl;
     }
 
     public function handleRequest() {
@@ -18,8 +20,10 @@ class Router {
         // Debugging output
         echo "Original Path: $path";
 
-        // Update path to remove /E-Lib if it exists
-        $path = str_replace('/E-Lib/app', '', $path);
+        // Update path to remove the base URL if it exists
+        if (strpos($path, $this->baseUrl) === 0) {
+            $path = substr($path, strlen($this->baseUrl));
+        }
 
         // Debugging output
         echo "Updated Path: $path";
