36451d986023110462c420e9673870cd893a97c6
nikmen <menounosnikitas@gmail.com>
2025-04-12 20:02:44 +0300

    feat: update file paths for stylesheets and add new CSS files for improved layout and design

--- app/views/search_results.php
+++ app/views/search_results.php
@@ -6,7 +6,7 @@
     <title>Search Results</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./styles/searchResults.css"> 
+    <link rel="stylesheet" href="../../public/styles/searchResults.css"> 
 </head>
 <body>
     <!-- Navbar -->
--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL & ~E_DEPRECATED);
 
 use App\Router\Router;
-$baseUrl = '/localhost:5500'; // Set your base URL here
+$baseUrl = ''; // Set your base URL here
 $router = new Router($baseUrl); 
 $router->handleRequest();
