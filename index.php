3541105370de94afdb674022049b2b4dae261eb7
nikmen <menounosnikitas@gmail.com>
2025-04-12 16:45:09 +0300

    fix: update error reporting settings and reference hashes in installed.php for consistency

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED);
 
 use App\Router\Router;
 $baseUrl = '/E-Lib';
