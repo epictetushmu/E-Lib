10312d1054ba0ab0c48091c843414db492628404
Nikitas Menounos <menounosnikitas@gmail.com>
2025-04-17 00:44:54 +0300

    fix: correct path for DatabaseInterface include in index.php

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@ if (file_exists($baseRouterPath)) {
 
 require_once __DIR__ . '/../App/Router/PageRouter.php';
 require_once __DIR__ . '/../App/Router/ApiRouter.php';
-require_once __DIR__ . '/../App/Includes/DatabaseInterface.php';
+require_once __DIR__ . '/../App/DataBase/DatabaseInterface.php';
 require_once __DIR__ . '/../App/Database/JsonDatabase.php';
 require_once __DIR__ . '/../App/Database/MongoDatabase.php';
 require_once __DIR__ . '/../App/Includes/Environment.php';
