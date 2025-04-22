2ac3f17c2fe93fb8576ee1b46179576ced40eccd
nikmen <menounosnikitas@gmail.com>
2025-04-22 14:01:17 +0300

    refactor: Update routing patterns for book and search endpoints to use MongoDB ObjectId format

--- App/Router/PageRouter.php
+++ App/Router/PageRouter.php
@@ -29,9 +29,9 @@ class PageRouter {
             ['path' => '/signup', 'handler' => [new PageController(), 'signupForm']],
             ['path' => '/view-books', 'handler' => [new PageController(), 'viewBooks']],
             ['path' => '/profile', 'handler' => [new PageController(), 'profile']],
-            ['path' => '/book/(\d+)', 'handler' => [new PageController(), 'viewBook']],
+            ['path' => '/book/([0-9a-f]{24})', 'handler' => [new PageController(), 'viewBook']],
             ['path' => '/add-book', 'handler' => [new PageController(), 'addBookForm']],
-            ['path' => '/search/(d+)', 'handler' => [new PageController(), 'searchBooks']],
+            ['path' => '/search/([0-9a-f]{24})', 'handler' => [new PageController(), 'searchBooks']],
             ['path' => '/error', 'handler' => [new PageController(), 'error']]
         ];            
     }
